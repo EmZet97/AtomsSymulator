e21428d6701f8fb427958eb73cd19ecb6e3b33d1
MattShah <mzawiski97@gmail.com>
2019-12-11 21:24:48 +0100

    First commit
    Projekt init

---
+++ AtomsSymulator/AtomsSymulator/Vector2D.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomsSymulator
+{
+    class Vector2D
+    {
+        private float x;
+        private float y;
+
+        public Vector2D()
+        {
+            this.x = 0.0f;
+            this.y = 0.0f;
+        }
+
+        public Vector2D(float x, float y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public float X
+        {
+            get { return x; }
+            set { this.x = value; }
+        }
+        public float Y
+        {
+            get { return y; }
+            set { this.y = value; }
+        }
+    }
+}
